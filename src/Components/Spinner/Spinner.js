fd2f806b250649600b3ef2ee48e18f4b984f8e1d
Hung Mai <hwngmvi@gmail.com>
2022-10-02 12:27:18 +0700

    loading

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { FadeLoader } from "react-spinners";
+import Lottie from "lottie-react";
+import rocketSpinner from "../../Assets/rocketSpinner.json";
 import { useSelector } from "react-redux";
 export default function Spinner() {
   // Tạo useSelector để kiểm tra isLoading từ redux cho comp Spinner
   let { isLoading } = useSelector((state) => state.spinnerReducer);
   // Kiểm tra isLoading render nếu == true
   return isLoading ? (
-    <div className='h-screen w-screen fixed left-0 top-0 bg-rose-900 flex justify-center bg-opacity-70 items-center z-50'>
-      <FadeLoader speedMultiplier='0.5' color='#E71D36' />
+    <div className='h-screen w-screen fixed left-0 top-0 bg-rose-300 flex justify-center bg-opacity-70 items-center z-50'>
+      <Lottie height={400} width={400} animationData={rocketSpinner}></Lottie>{" "}
     </div>
   ) : (
     <></>
